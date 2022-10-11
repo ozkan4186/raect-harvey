d640b76986b2aae12e256fe9545608444ff2a6e7
özkan demir <ozkanndemir2009@gmail.com>
2022-10-12 01:06:22 +0300

    file

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.scss'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <a href="About us">About us</a>
+        <a href="For you">For you</a>
+        <a href="Services">Services</a>
+        <a href="Blog">Blog</a>
+        <a href="Contact">Contact</a>
+    </div>
+  )
+}
+
+export default Navbar
